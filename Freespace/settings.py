98bf81b32aea80f48728e8f1e765989a4742497d
Zenen <ztreadwe@uwo.ca>
2020-07-15 13:09:43 -0400

    Update settings.py

--- Freespace/settings.py
+++ Freespace/settings.py
@@ -1,3 +1,5 @@
+# This settings file is out of date and does not correspond to the actual website
+
 """
 Django settings for Freespace project.
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ukupwe&-13@)4^!*#9t8x+kyknlzgh*+5n$!0w6ej20xtktxi3'
+SECRET_KEY = # It's a secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
